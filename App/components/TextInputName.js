4b8cb226bdb6804454ca428298f09e5ed0504dc6
Luis R. Díaz Muñiz <luisrdm@gmail.com>
2022-09-26 16:24:09 -0300

    Minor changes

--- App/components/TextInputName.js
+++ App/components/TextInputName.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Label, TextInput, Text } from 'react-native';
+import { View, TextInput, Text } from 'react-native';
 
 const TextInputName = () => {
     const [textName, setTextName]= useState('');
@@ -7,14 +7,11 @@ const TextInputName = () => {
     return (
         <View style={{padding: 10}}>
             <Text>Ingresá un nombre para el gato.</Text>
-            {/* <Label>
-                Nombre */}
-                <TextInput 
-                    style={{borderWidth: 1, borderColor: '#3333ff', padding: 10, fontSize: 18 }}
-                    placeholder='Ingresá el nombre del gato.'
-                    onChangeText={newText => setTextName(newText)}
-                />
-            {/* </Label> */}
+            <TextInput 
+                style={{borderWidth: 1, borderColor: '#3333ff', padding: 10, fontSize: 18 }}
+                placeholder='Ingresá el nombre del gato.'
+                onChangeText={newText => setTextName(newText)}
+            />
             <Text style={{padding: 10, fontSize: 18}}>El nombre ingresado es: {textName}</Text>
         </View>
     );
